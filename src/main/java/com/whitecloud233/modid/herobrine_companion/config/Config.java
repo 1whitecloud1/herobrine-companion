2bbc3fb5537ae00500216bbacd5d49343ef62811
1whitecloud1 <1837877751@qq.com>
2026-03-03 13:39:48 +0800

    add some config

--- src/main/java/com/whitecloud233/modid/herobrine_companion/config/Config.java
+++ src/main/java/com/whitecloud233/modid/herobrine_companion/config/Config.java
@@ -4,7 +4,6 @@ import com.mojang.logging.LogUtils;
 import com.whitecloud233.modid.herobrine_companion.HerobrineCompanion;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import org.slf4j.Logger;
@@ -13,9 +12,6 @@ import org.slf4j.Logger;
 public class Config {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-
-    // Renamed keys to "_v2" to force config refresh (ignoring old config file values)
-    // 更改了键名以强制刷新配置（忽略旧配置文件的值）
 
     public static final ForgeConfigSpec.BooleanValue POEM_OF_THE_END_EXPLOSION = BUILDER
             .comment("Whether the Poem of the End weapon causes explosions in its broken state")
@@ -27,16 +23,31 @@ public class Config {
             .comment("是否启用 Herobrine 的怪物臣服AI")
             .define("heroKingAuraEnabled_v2", true);
 
-    // 在 Config.java 中
-    public static final ForgeConfigSpec SPEC = BUILDER.build(); // 添加 public
+    public static final ForgeConfigSpec.BooleanValue HERO_BLOCK_RESTORATION = BUILDER
+            .comment("Whether Herobrine automatically restores broken blocks")
+            .comment("Herobrine是否自动修复被破坏的方块")
+            .define("heroBlockRestoration_v2", false);
+
+    // 【新增】掉落物清理配置项
+    public static final ForgeConfigSpec.BooleanValue HERO_CLEAN_ITEMS = BUILDER
+            .comment("Whether Herobrine automatically cleans up dropped items")
+            .comment("Herobrine是否自动清理地上的掉落物")
+            .define("heroCleanItems_v2", false); // 默认开启
+
+    public static final ForgeConfigSpec SPEC = BUILDER.build();
     public static boolean poemOfTheEndExplosion;
     public static boolean heroKingAuraEnabled;
+    public static boolean heroBlockRestoration;
+    public static boolean heroCleanItems; // 【新增】
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
         poemOfTheEndExplosion = POEM_OF_THE_END_EXPLOSION.get();
         heroKingAuraEnabled = HERO_KING_AURA_ENABLED.get();
-        LOGGER.info("Herobrine Companion Config Loaded: Explosion={}, Aura={}", poemOfTheEndExplosion, heroKingAuraEnabled);
+        heroBlockRestoration = HERO_BLOCK_RESTORATION.get();
+        heroCleanItems = HERO_CLEAN_ITEMS.get(); // 【新增】
+        LOGGER.info("Herobrine Companion Config Loaded: Explosion={}, Aura={}, BlockRestoration={}, CleanItems={}",
+                poemOfTheEndExplosion, heroKingAuraEnabled, heroBlockRestoration, heroCleanItems);
     }
 }
 
